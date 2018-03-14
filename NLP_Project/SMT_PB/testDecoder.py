4df79813da0d841036f307073fb6ac4e3a89f360
Mihail <m.nacev@yahoo.com>
2018-03-14 15:57:00 +0100

    Phrase StackDecoder final fixes + add BLEU cummulative bigram score

--- NLP_Project/SMT_PB/testDecoder.py
+++ NLP_Project/SMT_PB/testDecoder.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from math import log
 import csv
 from nltk.translate import PhraseTable, StackDecoder
+from nltk.translate import bleu_score
 
 phrase_table=PhraseTable()
 with open('smt/phrase/phrases_full.txt', 'rt', encoding='utf8') as csvfile:
@@ -18,4 +19,38 @@ with open('smt/phrase/phrases_full.txt', 'rt', encoding='utf8') as csvfile:
 language_prob = defaultdict(lambda: -999.0)
 language_model = type('',(object,),{'probability_change': lambda self, context, phrase: language_prob[phrase], 'probability': lambda self, phrase: language_prob[phrase]})()
 stack_decoder=StackDecoder(phrase_table, language_model)
-print(stack_decoder.translate(['macedonian', 'women', 'and', 'children', 'church', 'because', 'sweep']))
+input_sentence="he said that macedonian women were victims involved in prostitution during a recent sweep in bulgaria"
+with open("translation.txt", 'a', encoding='utf8') as file:
+    print("Input sentence: "+input_sentence)
+    file.write("Input sentence: "+input_sentence+"\n")
+    input_words=input_sentence.split(' ')
+    translated_sentence=[]
+    temp_sentence=[]
+    previous_temp=[]
+    iteration_index=0
+    for i in range(0, len(input_words)):
+        temp_sentence.append(input_words[i])
+        translated_temp=stack_decoder.translate(temp_sentence)
+        print(translated_temp)
+        iteration_index+=1
+        file.write("Iteration "+str(iteration_index)+": "+str(' '.join(translated_temp))+"\n")
+        if len(translated_temp)==0:
+            temp_sentence=[]
+            for word in previous_temp:
+                translated_sentence.append(word)
+            translated_sentence.append('_')
+            translated_temp=[]
+        previous_temp=translated_temp
+    if len(translated_sentence)==0:
+        translated_sentence = translated_temp
+
+    print("Translated sentence: "+str(' '.join(translated_sentence)))
+    file.write("Translated sentence: "+str(' '.join(translated_sentence))+"\n")
+    mk_sentences=[]
+    with open('smt/phrase/MK-EN.lower.mk', 'rt', encoding='utf8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in reader:
+            mk_sentences.append(row)
+    file.write("BLEU score: %f" %(bleu_score.sentence_bleu(references=mk_sentences, hypothesis=translated_sentence, weights=(0.5, 0.5)))+"\n")
+    print("BLEU score: %f" %(bleu_score.sentence_bleu(references=mk_sentences, hypothesis=translated_sentence, weights=(0.5, 0.5))))
+
